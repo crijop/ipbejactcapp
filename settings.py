b09f6040c227cb6ef45f9de0f3221f066d1fbb07
91195604 <91195604@alunos.ipbeja.pt>
2012-08-02 18:03:17 +0100

    Altera??o dos caminhos para conteudo estatico Ex: CSS, Imagens

--- settings.py
+++ settings.py
@@ -53,7 +53,7 @@ MEDIA_ROOT = 'templates'#'/home/jasnau/Codigo/Django/ipbejactcapp/distro/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost/templates/'#'http://localhost/media/'
+MEDIA_URL = 'http://localhost/templates'#'http://localhost/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
