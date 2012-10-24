a8f6c6675b10fa5e78b2aecb8b3da853141c9ab0
91195608 <91195608@alunos.ipbeja.pt>
2012-10-24 18:53:27 +0100

    Adi??o de metodo filter departamento filter_abcd

--- urls.py
+++ urls.py
@@ -109,7 +109,7 @@ urlpatterns = patterns('',
     url(r'^distro/departamento/listDocentes/$', 'ipbejactcapp.distro.view_departamento.listDocentes',
         name='listDocentesDep'),
                        
-    url(r'^distro/departamento/listDocentes/filter_abc/$', 'ipbejactcapp.distro.view_departamento.filter_abc'),
+    url(r'^distro/departamento/listDocentes/filter_abc/$', 'ipbejactcapp.distro.view_departamento.filter_abcd'),
                        
     url(r'^distro/departamento/listDocentes/filter_cat/$', 'ipbejactcapp.distro.view_departamento.filter_cat'),
                        
