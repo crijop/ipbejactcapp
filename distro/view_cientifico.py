4cfc9c82aef6c298e58d6766b3022df27c0007e1
91195608 <91195608@alunos.ipbeja.pt>
2012-10-30 16:18:01 +0000

    Cria??o dos templatesdo cientifico para gerar o XLS

--- distro/view_cientifico.py
+++ distro/view_cientifico.py
@@ -5,17 +5,32 @@ Created on 21 de Set de 2012
 
 @author: admin1
 '''
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+
+
 
 '''
 Inicio das vistas do Ciêntifico
 '''
+cientificoUserTeste = user_passes_test(lambda u:u.groups.filter(name='Cientifico').count(), login_url='')
+
+
 
 @login_required(redirect_field_name='login_redirectUsers')
+@cientificoUserTeste
 def indexCientifico(request):
     return render_to_response("cientifico/index.html",
+        locals(),
+        context_instance=RequestContext(request),
+        )
+    pass
+
+@login_required(redirect_field_name='login_redirectUsers')
+@cientificoUserTeste
+def criarXLS(request):
+    return render_to_response("cientifico/criar_xls.html",
         locals(),
         context_instance=RequestContext(request),
         )
