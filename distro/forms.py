81d4f99f19bf788e7729ce6c9a21d32f7d250729
Carlos Rijo Palma <carlosrijopalma@gmail.com>
2014-01-18 15:40:29 +0000

    Formulario da adicionar Docente

--- distro/forms.py
+++ distro/forms.py
@@ -12,26 +12,34 @@ from django.forms.models import ModelForm
 
 #Class de Teste para adicionar docentes.
 class AddDocenteForm(forms.Form):
-    nome_completo     = forms.CharField(max_length=300, label=u'Nome Completo')
-    departamento      = forms.ModelChoiceField(queryset=Departamento.objects.all())
-    escalao           = forms.IntegerField(required=False, initial= 100)
-    regime_exclusividade = forms.BooleanField(required=False, initial = True)
-    email = forms.EmailField(required = False, label=u'Email Institucional')
-    abreviatura       = forms.CharField()
+    nome_completo     = forms.CharField(max_length=300, label=u'Nome Completo', \
+                                        widget=forms.TextInput(attrs={'class':'form-control'}))
+    departamento      = forms.ModelChoiceField(queryset=Departamento.objects.all(), \
+                                               widget=forms.TextInput(attrs={'class':'form-control'}))
+    escalao           = forms.IntegerField(required=False, initial= 100, \
+                                           widget=forms.TextInput(attrs={'class':'form-control'}) )
+    regime_exclusividade = forms.BooleanField(required=False, initial = True, \
+                    widget=forms.CheckboxInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(required = False, label=u'Email Institucional', \
+                             widget=forms.TextInput(attrs={'class':'form-control'}))
+    abreviatura       = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 
 #Class que apresenta o formulário para
 #adicionar o docente
 class AdicionarDocenteForm(ModelForm):
     #widget=forms.TextInput(attrs={ 'required': 'true' })
-    nome_completo     = forms.CharField(max_length=300, label=u'Nome Completo')
-    email = forms.EmailField(required = False, label=u'Email Institucional')
-    abreviatura       = forms.CharField()
+    nome_completo     = forms.CharField(max_length=300, label=u'Nome Completo', \
+                                        widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(required = False, label=u'Email Institucional', \
+                             widget=forms.TextInput(attrs={'class':'form-control'}))
+    abreviatura       = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     
     departamento = forms.ModelChoiceField(Departamento.objects.all(),
-                                          widget = forms.Select(attrs = {'onchange':'testeSearch();'}))
+                                          widget = forms.Select(attrs = {'class':'form-control', 'onchange':'testeSearch();'}))
 
     regime_exclusividade = forms.BooleanField(required=False, initial = True, 
-                                              widget=forms.CheckboxInput(attrs={'onchange':'testeSearch();'}))
+                                              widget=forms.CheckboxInput(\
+                                            attrs={'class':'form-control', 'onchange':'testeSearch();'}))
     
     class Meta:
         model = Docente
