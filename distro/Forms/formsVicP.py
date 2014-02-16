8f657e1d20d1902a34a233d88e7e2782b803f90a
Carlos Rijo Palma <carlosrijopalma@gmail.com>
2014-02-16 01:09:56 +0000

    css dos forms da vicP

--- distro/Forms/formsVicP.py
+++ distro/Forms/formsVicP.py
@@ -14,12 +14,15 @@ class AdicionarCursoForm(ModelForm):
         tipoCurso = kwargs.pop('tipoCurso')
         super(AdicionarCursoForm, self).__init__(*args, **kwargs)
     
-        self.fields['nome'] = forms.CharField(max_length=100, label=u'Nome do Curso')
-        self.fields['abreviatura'] = forms.CharField(max_length=4)
-        self.fields['tipo_curso'] = forms.ModelChoiceField(widget=forms.Select(attrs={'class':"selectTipoCurso"\
+        self.fields['nome'] = forms.CharField(max_length=100, label=u'Nome do Curso', \
+                                        widget=forms.TextInput(attrs={'class':'form-control colorInput'}))
+        self.fields['abreviatura'] = forms.CharField(max_length=4, \
+                                        widget=forms.TextInput(attrs={'class':'form-control colorInput'}))
+        self.fields['tipo_curso'] = forms.ModelChoiceField(widget=forms.Select(attrs={'class':"selectTipoCurso form-control colorInput"\
             , 'onchange':'this.form.action=this.form.submit()'}), label="Tipo de Curso", \
             queryset=TipoCurso.objects.filter(id=tipoCurso), empty_label=None)
-        self.fields['semestre_letivos'] = forms.IntegerField(label="Semestres Letivos", initial=2)
+        self.fields['semestre_letivos'] = forms.IntegerField(label="Semestres Letivos", initial=2, \
+                                        widget=forms.TextInput(attrs={'class':'form-control colorInput'}))
         
     class Meta:
         model = Curso
