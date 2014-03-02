91110f37e65812a4bc01f0d9e05c49eb5f535a34
Carlos Rijo Palma <carlosrijopalma@gmail.com>
2014-03-02 14:20:18 +0000

    Editar users por parte do admin

--- distro/Forms/form_admin.py
+++ distro/Forms/form_admin.py
@@ -6,10 +6,24 @@ Created on 26/02/2014
 '''
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission, Group
+from django.utils.datastructures import SortedDict
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 
+#Cria o form de criação de grupos
+class AddGroupForm(forms.Form):
+        
+    name = forms.CharField(max_length=300, label=_(u'Nome do grupo'), widget=forms.TextInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+    
+    perm = forms.ModelMultipleChoiceField(queryset=Permission.objects.all(), label=_(u'Permissões'), widget=forms.SelectMultiple(attrs={'multiple class':'form-control colorInput tamanhoInputMultipleSelect'}))
+    
+    class Meta:
+        model = Group
+        pass
+
+
+# Formulario para adicionar utilizador
 class AddUserForm(forms.ModelForm):
     error_messages = {
         'duplicate_username': _(u"O nome de utilizador inserido já existe."),
@@ -68,29 +82,173 @@ class AddUserForm(forms.ModelForm):
     #         user.save()
     #     return user
     #===========================================================================
+    
 
 
-class UserChangeForm(forms.ModelForm):
-    username = forms.RegexField(
-        label=_("Username"), max_length=30, regex=r"^[\w.@+-]+$",
+class EditUserForm(forms.ModelForm):
+    error_messages = {
+        'duplicate_username': _(u"O nome de utilizador inserido já existe."),
+        'password_mismatch': _(u"Os dois campos da palavra-chave são diferentes."),
+    }
+    
+    first_name  = forms.CharField(required = False, max_length=150, label=u'Primeiro Nome', \
+                                        widget=forms.TextInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+    
+    last_name  = forms.CharField(required = False, max_length=150, label=u'Último Nome', \
+                                        widget=forms.TextInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+    email = forms.EmailField(max_length=300, label=_(u'E-mail'), widget=forms.TextInput(attrs={'class':'form-control tamanhoInput'}))
+    
+    username = forms.RegexField(label=_("Nome Utilizador"), max_length=30,
+        regex=r'^[\w.@+-]+$',
         help_text = _("Required. 30 characters or fewer. Letters, digits and "
                       "@/./+/-/_ only."),
         error_messages = {
             'invalid': _("This value may contain only letters, numbers and "
-                         "@/./+/-/_ characters.")})
-    password = ReadOnlyPasswordHashField(label=_("Password"),
-        help_text=_("Raw passwords are not stored, so there is no way to see "
-                    "this user's password, but you can change the password "
-                    "using <a href=\"password/\">this form</a>."))
-
-    def clean_password(self):
-        return self.initial["password"]
-
+                         "@/./+/-/_ characters.")}, widget=forms.TextInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+    
+    def __init__(self, username, *args, **kwargs):
+        super(EditUserForm, self).__init__(*args, **kwargs)
+        self.username = username
+    
+    
     class Meta:
         model = User
+        fields = ("first_name", "last_name", "email", "username",)
 
-    def __init__(self, *args, **kwargs):
-        super(UserChangeForm, self).__init__(*args, **kwargs)
-        f = self.fields.get('user_permissions', None)
-        if f is not None:
-            f.queryset = f.queryset.select_related('content_type')
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        print "ddddddddd", username, self.username
+        if self.username != username:
+            try:
+                User.objects.get(username=username)
+            except User.DoesNotExist:
+                return username
+            raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+# Actualizar Password.
+class SetPasswordForm(forms.Form):
+    """
+    A form that lets a user change set his/her password without entering the
+    old password
+    """
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    new_password1 = forms.CharField(label=_("New password"),
+                                    widget=forms.PasswordInput(attrs={'class':'form-control colorInput tamanhoInput'}) )
+    new_password2 = forms.CharField(label=_("New password confirmation"),
+                                    widget=forms.PasswordInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(SetPasswordForm, self).__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'])
+        return password2
+
+    def save(self, commit=True):
+        self.user.set_password(self.cleaned_data['new_password1'])
+        if commit:
+            self.user.save()
+        return self.user
+
+# FORM para os utilizadores poderem alterar a sua passord
+# ainda nao esta a ser chamado.
+class PasswordChangeForm(SetPasswordForm):
+    """
+    A form that lets a user change his/her password by entering
+    their old password.
+    """
+    error_messages = {
+        'password_incorrect': _("Your old password was entered incorrectly. "
+                                "Please enter it again."),
+    }
+    old_password = forms.CharField(label=_("Old password"),
+                                   widget=forms.PasswordInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+
+    def clean_old_password(self):
+        """
+        Validates that the old_password field is correct.
+        """
+        old_password = self.cleaned_data["old_password"]
+        if not self.user.check_password(old_password):
+            raise forms.ValidationError(
+                self.error_messages['password_incorrect'])
+        return old_password
+
+PasswordChangeForm.base_fields = SortedDict([
+    (k, PasswordChangeForm.base_fields[k])
+    for k in ['old_password', 'new_password1', 'new_password2']
+])
+
+
+# Reset a password por parte do administrador.
+class PasswordChangeFormReset(forms.Form):
+    """
+    A form used to change the password of a user in the admin interface.
+    """
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    password1 = forms.CharField(label=_("Password"),
+                                widget=forms.PasswordInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+    password2 = forms.CharField(label=_("Password (again)"),
+                                widget=forms.PasswordInput(attrs={'class':'form-control colorInput tamanhoInput'}))
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(PasswordChangeFormReset, self).__init__(*args, **kwargs)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'])
+        return password2
+
+    def save(self, commit=True):
+        """
+        Saves the new password.
+        """
+        self.user.set_password(self.cleaned_data["password1"])
+        if commit:
+            self.user.save()
+        return self.user
+
+
+
+
+#===============================================================================
+# class UserChangeForm(forms.ModelForm):
+#     username = forms.RegexField(
+#         label=_("Username"), max_length=30, regex=r"^[\w.@+-]+$",
+#         help_text = _("Required. 30 characters or fewer. Letters, digits and "
+#                       "@/./+/-/_ only."),
+#         error_messages = {
+#             'invalid': _("This value may contain only letters, numbers and "
+#                          "@/./+/-/_ characters.")})
+#     password = ReadOnlyPasswordHashField(label=_("Password"),
+#         help_text=_("Raw passwords are not stored, so there is no way to see "
+#                     "this user's password, but you can change the password "
+#                     "using <a href=\"password/\">this form</a>."))
+# 
+#     def clean_password(self):
+#         return self.initial["password"]
+# 
+#     class Meta:
+#         model = User
+# 
+#     def __init__(self, *args, **kwargs):
+#         super(UserChangeForm, self).__init__(*args, **kwargs)
+#         f = self.fields.get('user_permissions', None)
+#         if f is not None:
+#             f.queryset = f.queryset.select_related('content_type')
+#===============================================================================
--- distro/view_admin.py
+++ distro/view_admin.py
@@ -4,13 +4,15 @@ Created on 23/02/2014
 
 @author: Carlos Rijo Palma & António Urbano Baião
 '''
-from distro.Forms.form_admin import AddUserForm
+from distro.Forms.form_admin import AddUserForm, AddGroupForm, EditUserForm, \
+    PasswordChangeForm, PasswordChangeFormReset
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import Group, User, Permission
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.template.response import TemplateResponse
 from django.utils.translation import ugettext, ugettext_lazy as _
 # index do administrador
 @login_required
@@ -40,11 +42,45 @@ def listGroup(request):
         locals(),
         context_instance=RequestContext(request),
         )
+    
+    
+# Cria o grupo na tabela Group
+@login_required(redirect_field_name = 'login_redirectUsers') 
+def addGroup(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = AddGroupForm(request.POST)
+           
+        if form.is_valid():
+            nomeGroup = form.cleaned_data['name']
+            tablegroup = Group(name = nomeGroup,
+                        )
+               
+            tablegroup.save()       
+            
+            # Relaciona as permissoes com o grupo       
+            for p in form.cleaned_data['perm']:
+                # relaciona a permissao ao grupo
+                g = Permission.objects.get(name = p.name)
+                 
+                # get last id inserted
+                last_id = Group.objects.latest('id')
+                # atualiza na db
+                g.group_set.add(last_id)   
+                        
+    else:
+        form = AddGroupForm()
+    return render_to_response("administrador/Groups/addGroup.html",
+        locals(),
+        context_instance = RequestContext(request),
+        )
+     
+    pass
 
 
 
 # Adicionar Utilizadores de utilizadores
 def addUsers(request, *args, **kwargs):
+    varTipoTemplate = "Add"
     if request.method == 'POST':
         form = AddUserForm(request.POST)
         if form.is_valid():
@@ -54,24 +90,56 @@ def addUsers(request, *args, **kwargs):
                         email = form.cleaned_data['email'],
                         password = form.cleaned_data['password1'],
                         )
-            
             tableuser.set_password(form.cleaned_data['password1'])
-               
             tableuser.save()
-            
-            
-            
-            
-            
-            #===================================================================
-            # msg = ugettext('Password changed successfully.')
-            # messages.success(request, msg)
-            # return HttpResponseRedirect('..')
-            #===================================================================
     else:
         form = AddUserForm()
     
-    template = "administrador/Users/addUser.html"
+    template = "administrador/Users/add_edit_User.html"
+    return render_to_response(template,
+        locals(),
+        context_instance=RequestContext(request),
+        )
+    
+# Adicionar Utilizadores de utilizadores
+def editUser(request, *args, **kwargs):
+    varTipoTemplate = "Edit"
+    idUser = kwargs["idUser"]
+    userEdit = User.objects.get(id = idUser)
+    if request.method == 'POST':
+        form = EditUserForm(userEdit.username, request.POST)
+        
+        if form.is_valid():
+            print "aaaaaaaaaa", form.cleaned_data['username']
+            User.objects.filter(id = idUser).update(username = form.cleaned_data['username'],
+                        first_name = form.cleaned_data['first_name'],
+                        last_name = form.cleaned_data['last_name'],
+                        email = form.cleaned_data['email'],
+                            )
+    else:
+        form = EditUserForm(userEdit.username, instance = userEdit)
+    
+    template = "administrador/Users/add_edit_User.html"
+    return render_to_response(template,
+        locals(),
+        context_instance=RequestContext(request),
+        )
+    
+# Fazer reset a password por parte do administrador
+def password_changeReset(request, *args, **kwargs):
+    idUser = kwargs["idUser"]
+    userEdit = User.objects.get(id = idUser)
+    nomeUser = userEdit.username
+    
+    password_change_form = PasswordChangeFormReset
+    if request.method == "POST":
+        form = password_change_form(user=userEdit, data=request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = password_change_form(user=userEdit)
+        
+    template = "administrador/Users/changePassword.html"
     return render_to_response(template,
         locals(),
         context_instance=RequestContext(request),
