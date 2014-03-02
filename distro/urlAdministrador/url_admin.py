2f684dead0badf24a4b09e8fdf641a342a427e98
Carlos Rijo Palma <carlosrijopalma@gmail.com>
2014-03-02 14:20:18 +0000

    Editar users por parte do admin

--- distro/urlAdministrador/url_admin.py
+++ distro/urlAdministrador/url_admin.py
@@ -17,6 +17,10 @@ urlpatterns = patterns('',
     # Url's destinados aos Grupos de utilizadores
     ####
     
+    # Adicionar grupos utilizadores
+    url(r'^addgroup/$', 'distro.view_admin.addGroup', 
+            name='addGroup'),
+    
     # Lista de grupos
     url(r'^lstgroup/$', 'distro.view_admin.listGroup', 
             name='listGroup'),
@@ -33,6 +37,16 @@ urlpatterns = patterns('',
     # Adicionar utilizadores
     url(r'^adduser/$', 'distro.view_admin.addUsers', 
             name='addUser'),
+    
+    # Editar utilizadores
+    url(r'^edituser/(?P<idUser>\d+)$', 'distro.view_admin.editUser', 
+            name='editUser'),
+    
+    url(r'^alterpwd/(?P<idUser>\d+)$', 'distro.view_admin.password_changeReset', 
+            name='alterPassword'),
+    
+                       
+                       
     
     
     # Lista de utilizadores
